637f690594746a010ccedc18031c65470ffe1ccf
Ansh Prajapati <anshprajapati074@gmail.com>
2026-01-20 18:50:28 +0530

    Add mongoose connection (db.js)

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,9 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const connectTodb = require("./db/db");
 app.use(cors());
+connectTodb();
 
 app.get("/", (req, res) => {
   res.send("hello there");
